b5f51181b8cd0ce56a8a80bcbcb8884ccc341f9f
tkouts <tkouts@innoscript.org>
2010-03-03 16:47:18 +0200

    new QuiX.utils.readCookie function

--- quix/utils/utils.js
+++ quix/utils/utils.js
@@ -30,6 +30,19 @@ QuiX.utils.uid = (
         };
     })();
 
+QuiX.utils.readCookie = function(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ')
+            c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0)
+            return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+
 //==============================================================================
 // Hash functions
 //==============================================================================
