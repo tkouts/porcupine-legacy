ae19b97ce451f024d33cd054df81599a9acf3f7f
tkouts <tkouts@innoscript.org>
2010-12-06 19:23:02 +0200

    - the edit widget is now static  - support for color and number column types

--- quix/ui/datagrid.js
+++ quix/ui/datagrid.js
@@ -7,21 +7,24 @@ QuiX.ui.DataGrid = function(/*params*/) {
     params.multiple = true;
     params.cellborder = params.cellborder || 1;
     params.cellpadding = params.cellpadding || 2;
-    params.onmousedown = QuiX.wrappers.eventWrapper(
-        QuiX.ui.DataGrid._onmousedown,
-        params.onmousedown);
 
     this.base = QuiX.ui.ListView;
     this.base(params);
     
     this.name = params.name;
-    this.hasSelector = true;
+    this.hasSelector = !(params.hasselector == false ||
+                         params.hasselector == 'false');
     this.editUndef = !(params.editundef == false ||
                        params.editundef == 'false');
 }
 
 QuiX.constructors['datagrid'] = QuiX.ui.DataGrid;
 QuiX.ui.DataGrid.prototype = new QuiX.ui.ListView;
+
+QuiX.ui.DataGrid.prototype.customEvents =
+    QuiX.ui.ListView.prototype.customEvents.concat(['onchange']);
+
+QuiX.ui.DataGrid.__editWidget = null;
 
 QuiX.ui.DataGrid.prototype.addHeader = function(params) {
     var oHeader = QuiX.ui.ListView.prototype.addHeader.apply(this, arguments);
@@ -32,7 +35,7 @@ QuiX.ui.DataGrid.prototype.addHeader = function(params) {
 
 QuiX.ui.DataGrid.prototype.addColumn = function(params) {
     var oCol = QuiX.ui.ListView.prototype.addColumn.apply(this, arguments);
-    oCol.editable = !(params.editable=='false' || params.editable == false);
+    oCol.editable = !(params.editable == 'false' || params.editable == false);
     return oCol;
 }
 
@@ -40,35 +43,51 @@ QuiX.ui.DataGrid.prototype.getValue = function(params) {
     return this.dataSet;
 }
 
-QuiX.ui.DataGrid.prototype._removeEditWidget = function() {
-    if (this.attributes.__editwidget) {
-        var w = this.attributes.__editwidget;
-        this.attributes.__editwidget = null;
+QuiX.ui.DataGrid._removeEditWidget = function() {
+    if (QuiX.ui.DataGrid.__editwidget) {
+        var w = QuiX.ui.DataGrid.__editwidget;
+        QuiX.ui.DataGrid.__editwidget = null;
         w.destroy();
-    }	
+    }
+}
+
+QuiX.ui.DataGrid.prototype.redraw = function(bForceAll /*, memo*/) {
+    QuiX.ui.DataGrid._removeEditWidget();
+    QuiX.ui.ListView.prototype.redraw.apply(this, arguments);
 }
 
 QuiX.ui.DataGrid.prototype.disable = function() {
-    this._removeEditWidget();
+    QuiX.ui.DataGrid._removeEditWidget();
     QuiX.ui.ListView.prototype.disable.apply(this, arguments);
 }
 
 QuiX.ui.DataGrid.prototype.refresh = function() {
-    this._removeEditWidget();
+    QuiX.ui.DataGrid._removeEditWidget();
     QuiX.ui.ListView.prototype.refresh.apply(this, arguments);
-    if (QuiX.utils.BrowserInfo.family == 'ie')  // strange bug in IE8
+    if (QuiX.utils.BrowserInfo.family == 'ie') {  // strange bug in IE8
         this.widgets[1].redraw();
+    }
 }
 
 QuiX.ui.DataGrid.prototype.edit = function(cell /*, focus*/) {
     var editValue,
         focus = (arguments.length == 2)? arguments[1]:true,
         w2 = null,
-        w2_type;
-    var idx = cell.cellIndex;
-    var ridx = QuiX.getParentNode(cell).rowIndex;
-    if (idx > 0 && idx < this.columns.length-1 && this.columns[idx].editable) {
-        editValue = this.dataSet[ridx][this.columns[idx].name];
+        w2_type,
+        idx = cell.cellIndex,
+        ridx = QuiX.getParentNode(cell).rowIndex,
+        editItem = this.dataSet[ridx];
+
+    QuiX.ui.DataGrid._removeEditWidget();
+
+    if (this.columns[idx].typeProvider) {
+        this.columns[idx].typeProvider(this.columns[idx], editItem);
+    }
+
+    if ((idx > (this.hasSelector? 0:-1)) &&
+            (idx < this.columns.length - (this.hasSelector? 1:0)) &&
+            this.columns[idx].editable) {
+        editValue = editItem[this.columns[idx].name];
         if (typeof editValue == 'undefined' && !this.editUndef)
             return null;
         var left = cell.offsetLeft;
@@ -76,50 +95,69 @@ QuiX.ui.DataGrid.prototype.edit = function(cell /*, focus*/) {
             left -= this.widgets[1].div.scrollWidth -
                     this.widgets[1].div.clientWidth;
         }
+        
         switch (this.columns[idx].columnType) {
             case 'optionlist':
                 w2 = new QuiX.ui.Combo({
-                    top : cell.offsetTop,
-                    left : left,
-                    width : cell.offsetWidth,
-                    height : cell.offsetHeight,
                     onchange : QuiX.ui.DataGrid._update
                 });
 
                 var options = this.columns[idx].options;
                 for (var i=0; i<options.length; i++) {
-                    if (editValue==options[i].value)
+                    if (editValue == options[i].value) {
                         options[i].selected = true;
-                    else
+                    }
+                    else {
                         options[i].selected = false;
+                    }
                     w2.addOption(options[i]);
                 }
+                break;
+            case 'color':
+                w2 = new QuiX.ui.ColorPicker({
+                    value : editValue,
+                    onchange : QuiX.ui.DataGrid._update,
+                    onkeyup: QuiX.ui.DataGrid._update
+                });
+                break;
+            case 'number':
+                w2 = new QuiX.ui.Spin({
+                    value : editValue,
+                    editable : true,
+                    min: this.columns[idx].min,
+                    max: this.columns[idx].max,
+                    step: this.columns[idx].step,
+                    onchange : QuiX.ui.DataGrid._update
+                });
                 break;
             case 'bool':
                 w2_type = 'checkbox';
             default:
                 w2 = new QuiX.ui.Field({
-                    top : cell.offsetTop,
-                    left : left,
-                    width : cell.offsetWidth,
-                    height : cell.offsetHeight,
                     value : editValue,
                     type : w2_type
                 });
-                if (w2_type == 'checkbox')
-                    w2.attachEvent('onchange', QuiX.ui.DataGrid._update)
-                else
-                    w2.attachEvent('onkeyup', QuiX.ui.DataGrid._update)
+                if (w2_type == 'checkbox') {
+                    w2.attachEvent('onchange', QuiX.ui.DataGrid._update);
+                }
+                else {
+                    w2.attachEvent('onkeyup', QuiX.ui.DataGrid._update);
+                }
         }
+        // fit into cell
+        w2.top = cell.offsetTop;
+        w2.left = left;
+        w2.width = cell.clientWidth;
+        w2.height = cell.clientHeight;
         // do not perform rtl xform
         w2._xformed = true;
         this.widgets[1].appendChild(w2);
-
         w2.redraw();
-        w2.div.scrollIntoView(false);
-        if (focus && w2.focus)
+        if (focus && w2.focus) {
             w2.focus();
-        this.attributes.__editwidget = w2;
+        }
+
+        QuiX.ui.DataGrid.__editwidget = w2;
         this.attributes.__rowindex = ridx;
         this.attributes.__cellindex = idx;
     }
@@ -128,10 +166,12 @@ QuiX.ui.DataGrid.prototype.edit = function(cell /*, focus*/) {
 
 QuiX.ui.DataGrid._onclick = function(evt, w) {
     var target = QuiX.getTarget(evt);
-    while (target && target.tagName != 'TD')
+    while (target && target.tagName != 'TD') {
         target = QuiX.getParentNode(target);
-    if (target)
+    }
+    if (target) {
         w.parent.edit(target);
+    }
 }
 
 QuiX.ui.DataGrid._onkeydown = function(evt, w) {
@@ -140,7 +180,7 @@ QuiX.ui.DataGrid._onkeydown = function(evt, w) {
         var r = dg.attributes.__rowindex;
         var c = dg.attributes.__cellindex;
         var rows = dg.list.rows;
-        var current_cell = rows[r].cells[c]; 
+        var current_cell = rows[r].cells[c];
         if (evt.shiftKey) {
             do {
                 current_cell = current_cell.previousSibling;
@@ -156,40 +196,42 @@ QuiX.ui.DataGrid._onkeydown = function(evt, w) {
             do {
                 current_cell = current_cell.nextSibling;
                 if (!current_cell) {
-                    if (r < rows.length - 1)
-                        current_cell = rows[r+1].cells[0];
-                    else
+                    if (r < rows.length - 1) {
+                        current_cell = rows[r + 1].cells[0];
+                    }
+                    else {
                         current_cell = rows[0].cells[0];
+                    }
                 }
             } while (!dg.columns[current_cell.cellIndex].editable)
         }
-        var cur_w = dg.attributes.__editwidget;
-        dg.edit(current_cell, QuiX.utils.BrowserInfo.family != 'op');
-        if (QuiX.utils.BrowserInfo.family == 'op') {
-            window.setTimeout(
-                function() {
-                    dg.attributes.__editwidget.focus();
-                }, 0);
-        }
-        if (cur_w)
-            cur_w.destroy();
+        dg.edit(current_cell, false);
+
+        window.setTimeout(
+            function() {
+                QuiX.ui.DataGrid.__editwidget.focus();
+            }, 0);
+
         QuiX.cancelDefault(evt);
+        QuiX.stopPropag(evt);
     }
 }
 
 QuiX.ui.DataGrid._update = function(evt, w) {
+    if (w) {
+        QuiX.stopPropag(evt);
+    }
     w = w || evt;
     var dg = w.parent.parent;
-    if (dg.attributes.__editwidget) {
+    if (QuiX.ui.DataGrid.__editwidget) {
         var r = dg.attributes.__rowindex;
         var c = dg.attributes.__cellindex;
         var cell = dg.list.firstChild.rows[r].cells[c];
-        var value = dg.attributes.__editwidget.getValue();
+        var value = QuiX.ui.DataGrid.__editwidget.getValue();
         dg.dataSet[r][dg.columns[c].name] = value;
-        dg._renderCell(cell, c, value);
+        dg._renderCell(cell, c, value, dg.dataSet[r]);
+        if (dg._customRegistry.onchange) {
+            dg._customRegistry.onchange(dg);
+        }
     }
-}
-
-QuiX.ui.DataGrid._onmousedown = function(evt, dg) {
-    dg._removeEditWidget();
 }
